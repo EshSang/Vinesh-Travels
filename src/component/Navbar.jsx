efd4b451cf8467cab358d3b605e4a96b3ca5f024
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-11-22 20:38:07 +0530

    change color vinesh & tours name

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "../styles/navbar.css";
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand className="navname" href="#home">Vinesh & Tours</Navbar.Brand>
+          <Navbar.Brand className="navname" href="#home">
+            Vinesh & Tours
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
