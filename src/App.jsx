a5e7011615ee84a6c6ddfc238686176dc7a06002
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-11-22 20:33:10 +0530

    add boostrap nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./component/Header";
+import Navbar from "./component/Navbar";
 
 const App = () => {
   return (
     <div className="toursite">
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
     </div>
   );
 };
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand className="navname" href="#home">Vinesh & Tours</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#reviews">Reviews</Nav.Link>
+            <Nav.Link href="#hire">Hire</Nav.Link>
+            <Nav.Link href="#contactus">Contact Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default ColorSchemesExample;
