7efe0910e51f65c228aaf4c239734186825ddfd0
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-11-23 14:15:09 +0530

    change css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "../styles/home.css";
-import img1 from "../images/img1.jpg";
+import img9 from "../images/img9.png";
 
 const Home = () => {
   return (
     <div>
       <div className="homelogo">
-        <img src={img1} alt="Description" />
+        <img src={img9} alt="Description" />
       </div>
       <div className="welcometext">
-           <h2> Welcome to Vinesh Tours</h2>
-        </div>
+        <h1> Welcome to </h1>
+        <h3> Vinesh Tours </h3>
+      </div>
     </div>
   );
 };
